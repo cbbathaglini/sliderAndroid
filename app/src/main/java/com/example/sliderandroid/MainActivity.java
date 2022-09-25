87acf9335e35033957b6e5fd8fc139fe44d50fcd
cbbathaglini <carine.bathaglini@inf.ufrgs.br>
2022-09-25 20:48:14 -0300

    sliders prontos utilizando material-intro

--- app/src/main/java/com/example/sliderandroid/MainActivity.java
+++ app/src/main/java/com/example/sliderandroid/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.sliderandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setFullscreen(true);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 1")
+                .description("Descrição 1")
+                .image(R.drawable.um)
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .scrollable(false)
+                .build());
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Título 2")
+                .description("Descrição 2")
+                .image(R.drawable.dois)
+                .background(R.color.white)
+                .backgroundDark(R.color.black)
+                .scrollable(false)
+                .build());
     }
 }
